69bedfefedffd69e6f167312d60c85e10f1e26e0
dd <78930706+mityadu@users.noreply.github.com>
2026-02-12 14:54:44 +0100

    Prerequisites installer

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 . scripts/print_utils.sh
+. scripts/prerequisites.sh
+
+# Prerequisites
+info "Installing prerequisites..."
+install_homebrew
+
+# ---
 
 chmod +x ./scripts/symlinks.sh
 ./scripts/symlinks.sh --link
---
+++ scripts/prerequisites.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CUR_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+. $CUR_SCRIPT_DIR/print_utils.sh
+
+install_homebrew() {
+	info "Installing Homebrew..."
+
+	export HOMEBREW_CASK_OPTS="--appdir=/Applications/"
+	if hash brew &>/dev/null; then
+		warning "Homebrew already installed."
+	else
+		sudo --validate
+		NONINTERACTIVE-1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		success "Homebrew installed successfully"
+	fi
+
+}
+
+if [ "$(basename "$0")" = "$(basename "${BASH_SOURCE[0]}")" ]; then
+	install_homebrew
+fi
